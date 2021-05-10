78b2a40fccf40039881fbafba9d4f9137fcd6610
Thomas Begush <tbegush@gmail.com>
2021-05-09 20:17:39 -0700

    laying out the framework

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+// create function called "fight"
+
+var playerName = window.prompt("Name your robot!");
+
+console.log(playerName);
+
+var playerOne = window.prompt("dafuq is you?");
+
+console.log(playerOne + " is a bitch!");
+window.alert(playerOne +" is a bitch!")
+
+// function fight () 
+// {    window.alert("The fight has begun!");   }
+//fight();
